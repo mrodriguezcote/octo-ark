a5f4e461d8b6f1549677c9167d033aaecdea9516
Marco Rodriguez <marcorodriguez@Marcos-Mac-mini.local>
2016-10-14 02:54:05 -0400

    adding sample cucumber test

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,15 @@
+//Step definitions
+
+module.exports = function () {
+  this.Given(/^I am on the Cucumber.js GitHub repository$/, function() {
+    return true;
+  });
+
+  this.When(/^I click on "([^"]*)"$/, function (text) {
+    return true;
+  });
+
+  this.Then(/^I should see "([^"]*)"$/, function (text) {
+    return true;
+  });
+};
