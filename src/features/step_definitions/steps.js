dd184118126bc2d3f4708722d3850c30fcfe20c5
Marco Rodriguez <marco.rodriguez@blueacorn.com>
2016-10-17 14:39:05 -0400

    setting up webdriverio, making different compose file for grid

--- src/features/step_definitions/steps.js
+++ src/features/step_definitions/steps.js
@@ -1,8 +1,23 @@
+var webdriverio = require('webdriverio');
+var options = {
+    desiredCapabilities: {
+        browserName: 'firefox',
+        host: '172.16.0.6',
+        port: 32780
+    }
+};
 
 
 module.exports = function () {
   this.Given(/^I am on the Cucumber.js GitHub repository$/, function() {
-    return true;
+    webdriverio
+        .remote(options)
+        .init()
+        .url('http://www.google.com')
+        .getTitle().then(function(title) {
+            console.log('Title was: ' + title);
+        })
+        .end();
   });
 
   this.When(/^I click on "([^"]*)"$/, function (text) {
