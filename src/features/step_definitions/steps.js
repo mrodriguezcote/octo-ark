4b421966276737d842226242151743335fb5043a
Marco Rodriguez <marco.rodriguez@blueacorn.com>
2016-10-17 13:29:58 -0400

    adding webdriverio npm dependency

--- src/features/step_definitions/steps.js
+++ src/features/step_definitions/steps.js
@@ -1,19 +1,15 @@
-//Step definitions
-var hipchat = require('hipchat-notifier').make('QA Cucumber-Docker','JARUTFwhp2LVWzTGp4pViINSGtWWgn3CLtTatKjk');
+
 
 module.exports = function () {
   this.Given(/^I am on the Cucumber.js GitHub repository$/, function() {
-  	hipchat.info('Step 1 Executed');
     return true;
   });
 
   this.When(/^I click on "([^"]*)"$/, function (text) {
-  	hipchat.info('Step 2 Executed');
     return true;
   });
 
   this.Then(/^I should see "([^"]*)"$/, function (text) {
-  	hipchat.info('Step 3 Executed');
     return true;
   });
 };
