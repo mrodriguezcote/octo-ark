e00118c673cac07f9c3cefd1ab5923503f6e130d
Marco Rodriguez <marco.rodriguez@blueacorn.com>
2016-11-05 02:24:04 -0400

    adding teardown route

--- express/server.js
+++ express/server.js
@@ -13,6 +13,15 @@ server.post('/payload', function (req, res) {
   );
 })
 
+server.get('/teardown', function (req, res) {
+  res.send('Stopping and removing containers');
+  cmd.get(
+      `
+          docker-compose down
+      `
+  );
+})
+
 server.listen(8000, function () {
   console.log('Build Test Server listening on port 8000')
 })
