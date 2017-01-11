c6170eaac5643112ff70ed8594879c03eafe0c90
Marco Rodriguez <marco.rodriguez@blueacorn.com>
2017-01-11 15:05:26 -0500

    removing web/ and moving results to data volume

--- pickles-entrypoint.sh
+++ pickles-entrypoint.sh
@@ -2,16 +2,16 @@
 
 [ "$1" != "delayed-pickles" ] && exec $@
 
-echo "running pickles -- outputs to src/pickles/web"
+echo "running pickles -- outputs to volume shared/pickles/web"
 echo "waiting on results..."
 
 # sleeping here to ensure cucumber gets a chance to remove results file
 sleep ${INITIAL_DELAY:-5}
 
-while [ ! -e /src/runner/.complete ]; do
+while [ ! -e /shared/runner/.complete ]; do
   sleep 1
 done
 
 echo "RESULTS FOUND -- RUNNING MONO!"
 
-mono lib/pickles.exe -f /src/runner/features -o /src/pickles/web -lr /src/runner/results.json -trfmt cucumberjson -df DHTML
+mono lib/pickles.exe -f /shared/runner/features -o /shared/pickles/web -lr /shared/runner/results.json -trfmt cucumberjson -df DHTML
